b0bc0abc778105529fd1af402da12f6a5227d0d7
Liza1040 <maxsimova2002eliza@yandex.ru>
2022-03-19 08:41:39 +0300

    update cI

--- linters/run.sh
+++ linters/run.sh
@@ -21,8 +21,8 @@ function check_log() {
     fi
 }
 
-# print_header "RUN cppcheck"
-# //check_log "cppcheck project --enable=all --inconclusive --error-exitcode=1 -I project/include --suppress=missingIncludeSystem" "\(information\)"
+print_header "RUN cppcheck"
+//check_log "cppcheck project --enable=all --inconclusive --error-exitcode=1 -I project/include --suppress=missingIncludeSystem" "\(information\)"
 
 print_header "RUN clang-tidy"
 check_log "clang-tidy project/src/* project/include/* -warnings-as-errors=* -extra-arg=-std=c99 -- -Iproject/include" "Error (?:reading|while processing)"
